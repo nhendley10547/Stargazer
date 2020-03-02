452c95943431441a8b584edf439d1e93a6fb9fb9
Nhan Q Tran <nt305748@gmail.com>
2020-03-02 13:59:27 -0600

    dfgo

--- Stargazer/Assets/Scripts/Equipment.cs
+++ Stargazer/Assets/Scripts/Equipment.cs
@@ -7,13 +7,12 @@ public abstract class Equipment : MonoBehaviour {
     public abstract void OnActivate();
 
     public virtual void OnEquip(Entity owner, Transform viewTransform) {
-        GetComponent<BoxCollider>().enabled = false;
+        
         this.owner = owner;
         this.transform.parent = viewTransform;
     }
 
     public virtual void OnDrop() {
-        GetComponent<BoxCollider>().enabled = true;
         this.owner = null;
         this.transform.parent = null;
     }
