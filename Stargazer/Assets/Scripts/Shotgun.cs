5da336a8d9ca276efe701bfb4d48cc132fd30c7b
Alex Dawson <aad4wson@gmail.com>
2020-03-04 16:29:26 -0600

    im a fucking genius

--- Stargazer/Assets/Scripts/Shotgun.cs
+++ Stargazer/Assets/Scripts/Shotgun.cs
@@ -7,14 +7,19 @@ public class Shotgun : Equipment {
     private const float MAX_RELOAD_TIME = .6f;
     private const float BULLET_SPEED = 12.0f;
     private const float BULLET_RANGE = 100.0f;
-    Vector3[] position = new Vector3[5];
-    GameObject[] bulletClone = new GameObject[5];
-
+    Vector3[] position = new Vector3[6];
+    GameObject[] bulletClone = new GameObject[6];
+    
     public override void OnActivate() {
+        int[] rnd = new int[6];
+        int[] rnd2 = new int[6];
+
         if (this.currentReloadTime <= 0) {
-            for (int i = 1; i < 5; i++)
+            for (int i = 0; i < 6; i++)
             {
-                Vector3 bulletDirection = Calculate.HeadingBasedDirection(this.transform.position, this.transform.eulerAngles + new Vector3(0, 20*i, 0));
+                rnd[i] = Random.Range(-5, 5);
+                rnd2[i] = Random.Range(-5, 5);
+                Vector3 bulletDirection = Calculate.HeadingBasedDirection(this.transform.position, this.transform.eulerAngles + new Vector3(rnd[i], rnd2[i], 0));
                 position[i] = this.transform.GetChild(0).position + bulletDirection * (.1f * i);
                 bulletClone[i] = Instantiate(bulletPrefab, position[i], this.transform.rotation) as GameObject;
                 bulletClone[i].transform.localScale = new Vector3(.05f, .05f, .05f);
