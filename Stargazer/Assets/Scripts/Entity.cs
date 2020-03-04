2374524a170291dce3a5d04c948b386321767825
Nhan Q Tran <nt305748@gmail.com>
2020-03-04 10:18:01 -0600

    add equipAction

--- Stargazer/Assets/Scripts/Entity.cs
+++ Stargazer/Assets/Scripts/Entity.cs
@@ -4,13 +4,7 @@ public class Entity : MonoBehaviour {
     public Vector3 velocity;
     public Vector3 direction;
     public Vector3 position;
-
-	protected LayerMask groundLayer; 
-    protected LayerMask itemLayer;
-
-    protected virtual void Start() {
-        groundLayer = LayerMask.NameToLayer("Ground");
-        itemLayer = LayerMask.NameToLayer("Item");
-        print("Hello");
-    }
+    public Equipment equipment;
+	
+	public LayerMask groundLayer; 
 }
--- Stargazer/Assets/Scripts/Equipment.cs
+++ Stargazer/Assets/Scripts/Equipment.cs
@@ -2,18 +2,8 @@ using UnityEngine;
 
 public abstract class Equipment : MonoBehaviour {
     
-    protected Entity owner;
+    public Entity ownerEntity;
 
     public abstract void OnActivate();
 
-    public virtual void OnEquip(Entity owner, Transform viewTransform) {
-        
-        this.owner = owner;
-        this.transform.parent = viewTransform;
-    }
-
-    public virtual void OnDrop() {
-        this.owner = null;
-        this.transform.parent = null;
-    }
 }
