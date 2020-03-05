a56c9606683eb63a2f4271a4d5866f33ce76eb15
Nhan Q Tran <nt305748@gmail.com>
2020-03-05 10:35:28 -0600

    >:)

--- Stargazer/Assets/Scripts/MonsterAI.cs
+++ Stargazer/Assets/Scripts/MonsterAI.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterAI : MonoBehaviour {
+public class MonsterAI : Entity {
 
 	private const int MAX_HEALTH = 10;
+	public GameObject revolverPrefab;
 
 	void Start () {
+		this.direction = this.transform.eulerAngles;
+		this.position = this.transform.position;
+
 		GetComponent<Health>().SetHealth(MAX_HEALTH);
-		GameObject revolverPrefab = (GameObject)Resources.Load("Prefabs/Revolver", typeof(GameObject));
 		Equipment revolver = Instantiate(revolverPrefab, Vector3.zero, Quaternion.Euler(0,0,0)).GetComponent<Equipment>();
 		GetComponent<EquipAction>().OnEquip(revolver, this.transform);
 	}
