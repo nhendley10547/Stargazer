d44b142ee506683590b41ff14aa9af24cf5b8f41
Alex Dawson <aad4wson@gmail.com>
2020-03-06 22:20:24 -0600

    stuff

--- Stargazer/Assets/Scripts/Weapons/Shotgun.cs
+++ Stargazer/Assets/Scripts/Weapons/Shotgun.cs
@@ -4,6 +4,7 @@ public class Shotgun : Equipment {
 
     public GameObject bulletPrefab;
     private float currentReloadTime = 0;
+    private float timeBetweenTwoShots = .5f;
     private const float MAX_RELOAD_TIME = 2.5f;
     private const float BULLET_SPEED = 12.0f;
     private const float BULLET_RANGE = 100.0f;
